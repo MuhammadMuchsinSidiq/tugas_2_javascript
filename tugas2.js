3fd31a5875ede5a17dce4f09472f5a522b851ee6
Sidiqmchn <msidiq410@gmail.com>
2021-09-13 16:03:23 +0700

    Tugas 2 Javascript
    number , string , boolean , null atau undifined dan array

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+var number = [1,2,3,4,5,6];
+var string = ["allam","dzakir","sholikhin"];
+let lebar = false;
+let tinggi = true;
+let volume
+let luas = null;
+
+
+console.log(number);
+console.log(string);
+console.log(lebar,tinggi);
+console.log(volume);
+console.log(luas);
